48ea2b3276da35e60fc8b2067a365ec499fd818f
fkubicek <66724409+fkubicek@users.noreply.github.com>
2022-12-05 20:03:51 +0100

    Update input.py
    Add file_readlines_no_newlines to aoc22_util.input

--- common/aoc22_util/input.py
+++ common/aoc22_util/input.py
@@ -1,3 +1,7 @@
 def file_readlines_stripped(path):
     with open(path, "r") as file:
         return map(lambda line: line.strip(), file.readlines())
+
+def file_readlines_no_newlines(path):
+    with open(path, "r") as file:
+        return map(lambda line: line.replace("\n", ""), file.readlines())
